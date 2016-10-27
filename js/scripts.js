8341d4fa363e02bddd86f068ae0220be3bd5222c
Blake Bless <blakebless7@gmail.com>
2016-10-26 17:24:28 -0700

    move stuff from old repo to new, because old was corrupt

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+  $('#page-1-link').click(function() {
+    $('.nav-bar').addClass('nav-right');
+    $('#page-1').addClass('page-expand');
+    $('.base').addClass('base-expand');
+  });
+
+  $('#page-2-link').click(function() {
+    $('.nav-bar').addClass('nav-right');
+    $('#page-2').addClass('page-expand');
+    $('.base').addClass('base-expand');
+  });
+
+  $('#page-3-link').click(function() {
+    $('#page-3').addClass('page-expand');
+    $('.nav-bar').addClass('nav-left');
+    $('.base').removeClass('base-expand');
+  });
+
+  $('#page-4-link').click(function() {
+    $('#page-4').addClass('page-expand');
+    $('.nav-bar').addClass('nav-left');
+    $('.base').removeClass('base-expand');
+  });
+
+  $('.nav-bar-img').click(function() {
+    $('.nav-bar').removeClass('nav-right');
+    $('.nav-bar').removeClass('nav-left');
+    $('#page-1').removeClass('page-expand');
+    $('#page-2').removeClass('page-expand');
+    $('#page-3').removeClass('page-expand');
+    $('#page-4').removeClass('page-expand');
+    $('.base').removeClass('base-expand');
+    $('.nav-bar-img').removeClass('nav-bar-img-center');
+    $('.nav-page-link').show();
+  });
+
+  $('.nav-page-link').click(function(){
+    if ($('.nav-bar').hasClass('nav-right') || $('.nav-bar').hasClass('nav-left')){
+      $('.nav-page-link').hide();
+      $('.nav-bar-img').addClass('nav-bar-img-center');
+    }
+  });
+});
