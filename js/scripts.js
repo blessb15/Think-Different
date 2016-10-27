c9867068b1432932befe5a43635a6430215d8dd2
Blake Bless <blakebless7@gmail.com>
2016-10-27 10:08:17 -0700

    have some sweet animations for page transitions

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,24 @@ $(document).ready(function(){
 
   $('#page-1-link').click(function() {
     $('.nav-bar').addClass('nav-right');
-    $('#page-1').addClass('page-expand');
+    $('#page-1').addClass('page-expand-left');
     $('.base').addClass('base-expand');
   });
 
   $('#page-2-link').click(function() {
     $('.nav-bar').addClass('nav-right');
-    $('#page-2').addClass('page-expand');
+    $('#page-2').addClass('page-expand-left');
     $('.base').addClass('base-expand');
   });
 
   $('#page-3-link').click(function() {
-    $('#page-3').addClass('page-expand');
+    $('#page-3').addClass('page-expand-right');
     $('.nav-bar').addClass('nav-left');
     $('.base').removeClass('base-expand');
   });
 
   $('#page-4-link').click(function() {
-    $('#page-4').addClass('page-expand');
+    $('#page-4').addClass('page-expand-right');
     $('.nav-bar').addClass('nav-left');
     $('.base').removeClass('base-expand');
   });
@@ -27,10 +27,10 @@ $(document).ready(function(){
   $('.nav-bar-img').click(function() {
     $('.nav-bar').removeClass('nav-right');
     $('.nav-bar').removeClass('nav-left');
-    $('#page-1').removeClass('page-expand');
-    $('#page-2').removeClass('page-expand');
-    $('#page-3').removeClass('page-expand');
-    $('#page-4').removeClass('page-expand');
+    $('#page-1').removeClass('page-expand-left');
+    $('#page-2').removeClass('page-expand-left');
+    $('#page-3').removeClass('page-expand-right');
+    $('#page-4').removeClass('page-expand-right');
     $('.base').removeClass('base-expand');
     $('.nav-bar-img').removeClass('nav-bar-img-center');
     $('.nav-page-link').show();
